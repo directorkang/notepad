7138a6ecfba7638f683efa56d0982b2d25297716
DirectorKang <lumlaoren@gmail.com>
2025-05-08 14:32:01 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Text Files|*.txt|All Files|*.*";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(open.FileName);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Text Files|*.txt|All Files|*.*";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(save.FileName, richTextBox1.Text);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();   
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (e.Control && e.KeyCode == Keys.S) 
+            {
+                saveToolStripMenuItem_Click(null, null);
+                e.Handled = true; 
+            }
+            else if (e.Control && e.KeyCode == Keys.O) 
+            {
+                openToolStripMenuItem_Click(null, null);
+                e.Handled = true;
+            }
+        }
+    }
+}
